2dc10895b15bf2892260cddcaa5d75c9abb3a380
David Lorenzo <EnforcerZhukov@users.noreply.github.com>
2018-03-21 18:25:54 +0100

    Update SkipSteamAgeVerification.user.js

--- SkipSteamAgeVerification.user.js
+++ SkipSteamAgeVerification.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Skip Steam Age Verification
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1_2018-03-21
 // @description  Bypass Steam date verification (Date of birth) by automatically submitting it as 1999/01/01
 // @author       EnforcerZhukov
-// @match        https://store.steampowered.com/agecheck/app/*
+// @encoding     utf-8
+// @match        http://store.steampowered.com/agecheck/app/*
 // @downloadURL  https://raw.githubusercontent.com/EnforcerZhukov/Steam-SkipAgeVerification/master/SkipSteamAgeVerification.user.js
 // @updateURL    https://raw.githubusercontent.com/EnforcerZhukov/Steam-SkipAgeVerification/master/SkipSteamAgeVerification.user.js
+// @homepage     https://github.com/EnforcerZhukov/Steam-SkipAgeVerification
 // @license      MIT
 // @grant        none
 // ==/UserScript==
